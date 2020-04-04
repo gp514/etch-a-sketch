370763dcc3970331b931ef661f44cc68482eac1b
gp514 <perownee@gmail.com>
2020-04-04 13:27:32 -0400

    add clear button with square count choice. colors add 10% black with each subsequent mouseenter

--- main.js
+++ main.js
@@ -1,20 +1,55 @@
-const squares = 16;
+let squares = 30; // default starting value
+let hue = 0;
 
 const container = document.querySelector(".container");
+const button = document.querySelector("button");
+
+button.addEventListener("click", reset);
+
 container.style.gridTemplateColumns = "repeat(" + squares + " 1fr)";
 container.style.gridTemplateRows = "repeat(" + squares + " 1fr)";
 
+init();
 
-for(let i = 1; i <=  squares; i++){
-    for(let j = 1; j <= squares; j++)
-    {
-        const etchSquare = document.createElement("div");
-        etchSquare.style.gridArea = i + " / " + j;
-        etchSquare.addEventListener("mouseenter", changeColor);
-        container.appendChild(etchSquare);
+function init(){
+    generateSquares(squares);
+}
+
+function reset(){
+    let clearPrompt = prompt("How many squares per side? (Max 64)");
+    if(clearPrompt >= 1 && clearPrompt <= 64) squares = clearPrompt;
+    else if(!clearPrompt) return;
+    clearSquares();
+    generateSquares(squares);
+}
+
+function clearSquares(){
+    while (container.hasChildNodes()){
+        container.removeChild(container.firstChild);
+    }
+}
+
+function generateSquares(numSquares){
+    container.style.gridTemplateColumns = "repeat(" + numSquares + " 1fr)";
+    container.style.gridTemplateRows = "repeat(" + numSquares + " 1fr)";
+
+    for(let i = 1; i <=  numSquares; i++){
+        for(let j = 1; j <= numSquares; j++)
+        {
+            const etchSquare = document.createElement("div");
+            etchSquare.style.gridArea = i + " / " + j;
+            etchSquare.dataset.customVariable = 0;
+            etchSquare.addEventListener("mouseenter", changeColor);
+            container.appendChild(etchSquare);
+        }
     }
 }
 
 function changeColor(){
-    this.style.backgroundColor = "black";
+    hue +=1;
+    lightnessFactor = this.dataset.customVariable;
+    console.log(lightnessFactor);
+    this.style.backgroundColor = "hsl(" + hue + ", 100%, " + (50 - lightnessFactor*5) + "%)";
+    let incrementLightness = parseInt(this.dataset.customVariable);
+    if(incrementLightness < 10) this.dataset.customVariable  = ++incrementLightness;
 }
